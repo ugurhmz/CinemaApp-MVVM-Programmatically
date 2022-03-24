cea647dbf2661b96dd7edffe4a88bda98ec14c82
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-03-25 01:15:49 +0300

    init

---
+++ CinemaApp-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaApp-MVVM
+//
+//  Created by ugur-pc on 25.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
